3c0ec5905da4c53fedbb968d9395ec1885c70a7d
nikey_es <nikey_es@yahoo.es>
2016-08-29 22:29:19 +0200

    Refractor console parameter to use Mono.Options

--- DotNetProjectsDependenciesViewer.ConsoleUI/Program.cs
+++ DotNetProjectsDependenciesViewer.ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,18 +14,57 @@ namespace DotNetProjectsDependenciesViewer.ConsoleUI
         static void Main(string[] args)
         {
             string rootFolder = string.Empty;
+            bool showHelp = false;
 
-            if (args.Length != 0)
+            var optionSet = new OptionSet() {
+                { "p|path=", "the root path of the solutions to search dependencies",
+                  p => rootFolder = p},
+                { "h|help",  "show this message and exit",
+                  v => showHelp = v != null }
+            };
+
+            List<string> parameters;
+            try
             {
-                rootFolder = args[0];
-                DependencyGenerator gen = new DependencyGenerator(rootFolder);
+                parameters = optionSet.Parse(args);
+                if (String.IsNullOrWhiteSpace(rootFolder))
+                {
+                    throw new OptionException("Missing required option -p=path", "p|path=");
+                }
+                else if (Directory.Exists(rootFolder) == false)
+                {
+                    throw new OptionException(String.Format("Invalid solutions path. Folder does not exist: {0}", rootFolder), "p|path=");
+                }
+
             }
-            else
+            catch (OptionException e)
             {
-                Console.WriteLine("Please enter a path with .Net projects or solutions.");
+                Console.Write("DotNetProjectsDependenciesViewer: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `DotNetProjectsDependenciesViewer --help' for more information.");
+                return;
             }
 
-            
+            if (showHelp)
+            {
+                ShowHelp(optionSet);
+                return;
+            }
+
+            DependencyGenerator gen = new DependencyGenerator(rootFolder);
+
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(String.Format("Go to {0} and open Dependencies.dgml with Visual Studio.", rootFolder));
+
         }
+
+        static void ShowHelp(OptionSet optionSet)
+        {
+            Console.WriteLine("Usage: DotNetProjectsDependenciesViewer [OPTIONS]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            optionSet.WriteOptionDescriptions(Console.Out);
+        }
+
     }
 }
